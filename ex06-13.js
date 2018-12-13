5c2b5f7c54e1834f3c53b76b93c608990d1f898b
zm17494 <zm17494@josai.ac.jp>
2018-12-13 00:08:22 -0500

    message

--- ex06-13.js
+++ ex06-13.js
@@ -12,7 +12,7 @@ function ThreeSecChange2(){
   setTimeout("imgchange2()",3000);
 }
 function imgchange2() {
-  document.getElementById('img13').src = './image/josai.png';
+  document.getElementById('img13').src = './image/ju.png';
 }
 
 //function ThreeSecBack() { 
--- ex07-8.js
+++ ex07-8.js
@@ -1,8 +1,7 @@
-
-function nazo2(){
-  if(document.getElementById('answer').value == '夢'){
-   alert('あたり！');
-  }else{
-    alert('はずれ!');
+function nazo2() {
+  if (document.getElementById('answer').value == '夢') {
+    alert('あたり！');
+  } else if (document.getElementById('answer').value === '') {
+    alert('はずれ！');
   }
 }
