34289c899f74beb579cba302c3a151ec4195a87b
zm17494 <zm17494@josai.ac.jp>
2018-12-16 21:00:43 -0500

    message

--- ex07-3.js
+++ ex07-3.js
@@ -1,5 +1,7 @@
 function add(){
-  var x = document.getElementById('input1').value;
-  var y = document.getElementById('input2').value;
-  alert(x+y);
+  var num1 = parseInt(document.getElementById('input1').value);
+  var num2 = parseInt(document.getElementById('input2').value);
+  alert(parseInt(num1)+parseInt(num2));
 }
+
+alert(num1+num2);
